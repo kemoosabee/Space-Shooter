425fffa9db8b8a2a870e5fdb1aa78876cf93a7de
Jia <jlee48@qub.ac.uk>
2020-05-11 23:33:53 +0100

    Third Commit

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private Player _player;
+    private Animator m_Animator;
+    [SerializeField]
+    private float _speed = -3.0f;
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
+        if(_player == null)
+        {
+            Debug.LogError("Player is null");
+        }
+        m_Animator = GetComponent<Animator>();
+
+        if(m_Animator == null)
+        {
+            Debug.LogError("The Animator is null");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate (new Vector3(0,-2,0) * Time.deltaTime);
+        transform.Translate (new Vector3(0,_speed,0) * Time.deltaTime);
 
         if(transform.position.y <= -4){
             transform.position = new Vector3(Random.Range(-9.0f,9.0f),4,0);
@@ -28,21 +42,28 @@ public class Enemy : MonoBehaviour
             Player player = other.transform.GetComponent<Player>();
 
             if(player!=null && player.ShieldState == true){
-                Destroy(this.gameObject);
+                _speed = 0f;
+                m_Animator.SetTrigger("OnEnemyDeath");
+                Destroy(this.gameObject,2.3f);
             }
             else if(player != null){
                 player.Damage();
-                Destroy(this.gameObject);
+                _speed = 0f;
+                m_Animator.SetTrigger("OnEnemyDeath");
+                Destroy(this.gameObject,2.3f);
             }
         }
 
         if(other.tag == "Laser")
         {
             Destroy(other.gameObject);
-            Destroy(this.gameObject);
             if(_player!=null){
                 _player.AddScore();
             }
+            m_Animator.SetTrigger("OnEnemyDeath");
+            _speed = 0f;
+            Destroy(this.gameObject,2.3f);
+
         }
     }
 }
